98ec31ff68e777e8f3b3c6838fa18973c63eb4ff
Ghosterbeef <vahnin24@gmail.com>
2023-05-20 22:19:27 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  {{ filtered }}
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from 'vue';
+
+const array = ref<{a: number; b: number}[] | null>([
+  {
+    a: 1,
+    b: 2,
+  },
+]);
+
+const filtered = computed(() => {
+  if (!array.value) {
+    return [];
+  }
+
+  return array.value.filter((item) => item.a < item.b);
+});
+</script>
